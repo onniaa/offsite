b058b8f1039757f52daaf0a372fc6211e6396ae3
Onni Amir <onniamir@Onnis-MacBook-Pro.local>
2025-01-10 11:01:15 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Welcome to My React App!</h1>
+        <p>This is a simple React app for learning purposes.</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
+
